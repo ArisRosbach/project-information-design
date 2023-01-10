74dead73e1bae7111df0751eae36768a2076e21c
Aris <aris.rosbach@hva.nl>
2023-01-10 10:13:06 +0100

    Add test scroll snap

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,34 @@ console.log('Hello, world!');
 let data = count(5, 5);
 
 console.log(data);
+
+
+
+const home = document.getElementById("item-home");
+const uitleg1 = document.getElementById("item-uitleg1");
+const uitleg2 = document.getElementById("item-uitleg2");
+const uitleg3 = document.getElementById("item-uitleg3");
+const button = document.getElementById("button");
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+
+button.addEventListener("click", () => {
+  console.log("button clicker");
+  uitleg1.scrollIntoView({ behavior: 'smooth', block: 'center' });
+});
+
+button1.addEventListener("click", () => {
+  console.log("button clicker");
+  uitleg2.scrollIntoView({ behavior: 'smooth', block: 'center' });
+});
+
+button2.addEventListener("click", () => {
+  console.log("button clicker");
+  uitleg3.scrollIntoView({ behavior: 'smooth', block: 'center' });
+});
+
+button3.addEventListener("click", () => {
+  console.log("button clicker");
+  home.scrollIntoView({ behavior: 'smooth', block: 'center' });
+});
